ac09c31bd0c5046f14e3255068816b1b314d04d0
Gabriel Almeida <gabriel@agenciaeplus.com.br>
2022-10-23 14:21:00 -0300

    finalizado revisao de react state e effect

--- React/dojo-blog/src/App.js
+++ React/dojo-blog/src/App.js
@@ -1,15 +1,26 @@
 import logo from "./logo.svg";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import Create from "./Components/Create";
+import BlogDetails from "./Components/BlogDetails";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-        <Navbar/>
-      <div className="content">
-        <Home/>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/"  element={<Home/>} />
+            <Route path="/create"  element={<Create/>} />
+            <Route path="/blogs/:id"  element={<BlogDetails/>} />
+            <Route path="*" element={<NotFound/>} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- React/dojo-blog/src/hooks/useFetch.js
+++ React/dojo-blog/src/hooks/useFetch.js
@@ -7,22 +7,21 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect( () => { // run in every render, if u put [] at second argument the useEffect run only once and if u put any other state in brackets useEffect only runs when this state change
-        fetch(url)
-        .then(res => {
-            console.log(res)
-            if(!res.ok){
-                throw Error('could not fetch the data for that resource')
-            }
-            return res.json()
-        })
-        .then(json => {
-            setIsPending(false)
-            setError(null)
-            setData(json)
-        })
-        .catch( err => {
-            setError(err.message)
-        })
+            fetch(url)
+            .then(res => {
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource')
+                }
+                return res.json()
+            })
+            .then(json => {
+                setIsPending(false)
+                setError(null)
+                setData(json)
+            })
+            .catch( err => {
+                setError(err.message)
+            })
     },[])
 
     return { data, isPending, error }
