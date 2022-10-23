bd38065e9f0eb8beff1df4eb00fc7cad592e2433
Gabriel Almeida <gabriel@agenciaeplus.com.br>
2022-10-23 17:23:09 -0300

    reading app using state, context, reducers and localstorage, just a little brief

---
+++ React/reading-app/src/components/BookList.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { BookContext } from "../context/BookContext";
+import BookDetails from './BookDetails'
+
+const BookList = () => {
+    const { books } = useContext(BookContext)
+    return books.length ? (
+        <div className='book-list'>
+            <ul>
+                {books && books.map(book => {
+                    return (<BookDetails book={book} key={book.id} /> )
+                })}
+            </ul>
+        </div>
+    ) : (
+        <div className="empty">No Books to ReadableByteStreamController. Hello free time :)</div>
+    )
+}
+ 
+export default BookList;
